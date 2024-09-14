0b3381c0f0222fe836694d3a47e645ec859f86e1
Sakeeb M Gadyal <128926541+sakeeb17@users.noreply.github.com>
2024-09-14 15:38:32 +0530

    Update database.py

--- frontend_using_streamlit/database.py
+++ frontend_using_streamlit/database.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb = mysql.connector.connect(host="localhost", user="root", password="", database="final_movie")
+mydb = mysql.connector.connect(host="localhost", user="root", password="sakeeb", database="movie_ticket")
 c = mydb.cursor()
 
 
